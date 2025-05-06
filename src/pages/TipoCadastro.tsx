43602cbb363f7b605e5cc75485ae728d12de5e52
Erick Cardoso <135283010+Erickncardoso@users.noreply.github.com>
2025-05-06 15:38:27 -0300

    Update TipoCadastro.tsx

--- src/pages/TipoCadastro.tsx
+++ src/pages/TipoCadastro.tsx
@@ -145,7 +145,7 @@ const TipoCadastro: React.FC = () => {
             <img src='/images/logo-sabara.png' alt="Logo Hospital Sabará" className="h-10 md:h-14" />
           </div>
           
-          <h1 className="text-3xl font-bold text-center mb-10 text-gray-800">Escolha o tipo do cadastro</h1>
+          <h1 className="text-3xl font-bold text-center mb-10 text-gray-800">Escolha o tipo do login</h1>
           
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             <div>
